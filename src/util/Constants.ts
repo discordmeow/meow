19f548f810de259901d7ae5ba7b8c693b689b7de
Cat66000 <none>
2020-07-22 00:36:32 +0200

    Replace Api with API_BASE_URL & API_VERSION (Constants)

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -1,5 +1,4 @@
-export const Api = {
-  BASE_URL: "https://discord.com/api",
-  VERSION: "v7",
-};
+export const API_BASE_URL = "https://discord.com/api/";
+export const API_VERSION = "v7";
+
 export const CDN_BASE_URL = "https://cdn.discordapp.com/";
