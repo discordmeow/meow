1714244c6f6a7d52182146ee02307800afd5336d
Cat66000 <none>
2020-07-22 01:05:54 +0200

    Create Channel model

---
+++ src/models/Channel.ts
@@ -0,0 +1,11 @@
+interface ChannelStructure {
+    id: string;
+  }
+  
+  export class Channel {
+    readonly id: string;
+    constructor(structure: ChannelStructure) {
+      this.id = structure.id
+    }
+  }
+  
