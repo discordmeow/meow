6db1b430686b8774f1bfec07fcc6d46140ff87a3
Cat66000 <none>
2020-07-22 00:36:07 +0200

    Add a condition in User model's constructor

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,30 +13,33 @@ interface ReceivedUserStructure {
 }
 
 export class User {
-  public readonly id: string;
-  public readonly username?: string;
-  public readonly discriminator?: string;
-  public readonly avatarHash?: string;
-  public readonly isBot?: boolean;
-  public readonly isSystem?: boolean;
-  public readonly hasMFA?: boolean;
-  public readonly locale?: string;
-  // public readonly flags?: number;
-  public readonly premiumType?: number;
-  public readonly publicFlags?: number;
+  readonly id: string;
+  readonly username?: string;
+  readonly discriminator?: string;
+  readonly avatarHash?: string;
+  readonly isBot?: boolean;
+  readonly isSystem?: boolean;
+  readonly hasMFA?: boolean;
+  readonly locale?: string;
+  // readonly flags?: number;
+  readonly premiumType?: number;
+  readonly publicFlags?: number;
 
   constructor(structure: ReceivedUserStructure) {
     this.id = structure.id;
     this.username = structure.username;
-    this.discriminator = structure.discriminator;
-    this.avatarHash = structure.avatar;
-    this.isBot = structure.bot;
-    this.isSystem = structure.system;
-    this.hasMFA = structure.mfa_enabled;
-    this.locale = structure.locale;
-    // this.flags = structure.flags;
-    this.premiumType = structure.premium_type;
-    this.publicFlags = structure.public_flags;
+
+    if (!this.isPartial) {
+      this.discriminator = structure.discriminator;
+      this.avatarHash = structure.avatar;
+      this.isBot = structure.bot;
+      this.isSystem = structure.system;
+      this.hasMFA = structure.mfa_enabled;
+      this.locale = structure.locale;
+      // this.flags = structure.flags;
+      this.premiumType = structure.premium_type;
+      this.publicFlags = structure.public_flags;
+    }
   }
 
   get isPartial(): boolean {
