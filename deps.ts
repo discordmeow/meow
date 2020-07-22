aa57b0d354972c4b4ef0a910b2615cbbcf1d0e7d
Cat66000 <none>
2020-07-22 18:38:54 +0200

    Fix deps.ts

--- deps.ts
+++ deps.ts
@@ -1,5 +1,6 @@
 export {
+  WebSocket,
   connectWebSocket,
-  isWebSocketCloseEvent, WebSocket
+  isWebSocketCloseEvent,
 } from "https://deno.land/std@0.61.0/ws/mod.ts";
 export { Evt } from "https://deno.land/x/evt@1.7.14/mod.ts";
