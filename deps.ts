496e0d7bb682831f1176f2d124193140f324229f
Cat66000 <none>
2020-07-22 21:02:30 +0200

    Add isWebSocketPingEvent/PongEvent in deps.ts

--- deps.ts
+++ deps.ts
@@ -2,5 +2,7 @@ export {
   WebSocket,
   connectWebSocket,
   isWebSocketCloseEvent,
+  isWebSocketPingEvent,
+  isWebSocketPongEvent,
 } from "https://deno.land/std@0.61.0/ws/mod.ts";
 export { Evt } from "https://deno.land/x/evt@1.7.14/mod.ts";
