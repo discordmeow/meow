90ba497fc7bc0bc3149b52b9da0a18b874526d54
Cat66000 <none>
2020-07-22 21:04:03 +0200

    Add client#users, client#ws and client#events

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,9 +1,11 @@
 import { UserManager } from "../managers/UserManager.ts";
+import { WebSocketHandler } from "../network/WebSocketHandler.ts";
+import * as eventsHandlers from "./Events.ts";
 
 export class Client {
-  readonly users: UserManager;
+  readonly users = new UserManager(this);
+  readonly ws = new WebSocketHandler(this);
+  events = eventsHandlers;
 
-  constructor() {
-    this.users = new UserManager(this);
-  }
+  constructor() {}
 }
