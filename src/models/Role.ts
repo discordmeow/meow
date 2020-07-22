2ac5c8291685dc5537603604c209c8f1adc1fe59
n1c00o <34602094+Nicoxx45@users.noreply.github.com>
2020-07-22 18:18:11 +0200

    Role model

---
+++ src/models/Role.ts
@@ -0,0 +1,36 @@
+import { Client } from '../client/Client.ts';
+import { Guild } from './Guild.ts';
+
+export interface ReceivedRoleStructure {
+  id: string;
+  name: string;
+  color: number;
+  hoist: boolean;
+  position: number;
+  permissions: number;
+  managed: boolean;
+  mentionable: boolean;
+}
+
+export class Role {
+  public readonly id: string;
+  public name: string;
+  public color: number;
+  public hoist: boolean;
+  public position: number;
+  public permissions: number;
+  public managed: boolean;
+  public mentionable: boolean;
+
+  constructor(structure: ReceivedRoleStructure, public guild: Guild, public client: Client) {
+    this.id = structure.id;
+    this.name = structure.name;
+    this.color = structure.color;
+    this.hoist = structure.hoist;
+    this.position = structure.position;
+    this.permissions = structure.permissions;
+    this.managed = structure.managed;
+    this.mentionable = structure.mentionable;
+  }
+}
+
