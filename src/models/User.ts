cc2f8c97e7f6e7d9faf6260e47c5e1aee94e7803
n1c00o <34602094+Nicoxx45@users.noreply.github.com>
2020-07-22 18:29:40 +0200

    Added documentation

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,27 @@
 import { Client } from '../client/Client.ts';
 
 export class User {
+  /** The user's ID */
   public readonly id: string;
+  /** The user's username, not unique across the platform */
   public username?: string;
+  /** The user's 4-digit discord-tag */
   public discriminator?: string;
+  /** The user's avatar hash */
   public avatarHash?: string;
+  /** Whether the user belongs to an OAuth2 application */
   public bot?: boolean;
+  /** Whether the user is an Official Discord System user (part of the urgent message system) */
   public system?: boolean;
+  /** Whether the user has two factor enabled on their account */
   public mfaEnabled?: boolean;
+  /** The user's chosen language option */
   public locale?: string;
+  // /** The flags on a user's account */
   // readonly flags?: number;
+  /** The type of Nitro subscription on a user's account */
   public premiumType?: number;
+  /** The public flags on a user's account */
   public publicFlags?: number;
 
   constructor(structure: any, public client: Client) {
