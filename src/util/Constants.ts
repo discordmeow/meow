de4a7512b81ad930828649b0575869f697d5b7c3
Cat66000 <none>
2020-07-22 16:57:25 +0200

    Add constant GATEWAY_BASE_URL Remove / at the end of URLs

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -1,4 +1,6 @@
-export const API_BASE_URL = "https://discord.com/api/";
+export const API_BASE_URL = "https://discord.com/api";
 export const API_VERSION = "v7";
 
-export const CDN_BASE_URL = "https://cdn.discordapp.com/";
+export const CDN_BASE_URL = "https://cdn.discordapp.com";
+
+export const GATEWAY_BASE_URL = "wss://gateway.discord.gg";
