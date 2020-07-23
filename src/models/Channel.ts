eab58bb34ef0cb2f6fd72d6c5b24c2456d978820
Cat66000 <none>
2020-07-23 21:42:56 +0200

    Add missing parameter in Channel's constructor

--- src/models/Channel.ts
+++ src/models/Channel.ts
@@ -1,10 +1,12 @@
+import { Client } from "../client/Client.ts";
+
 interface ChannelStructure {
   id: string;
 }
 
 export class Channel {
   readonly id: string;
-  constructor(structure: ChannelStructure) {
+  constructor(structure: ChannelStructure, public client: Client) {
     this.id = structure.id;
   }
 }
