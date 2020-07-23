2dddf3eb5135e579033cbc89c6f48d5c8abcbf4a
n1c00o <34602094+Nicoxx45@users.noreply.github.com>
2020-07-23 22:48:57 +0200

    Channel model

--- src/models/Channel.ts
+++ src/models/Channel.ts
@@ -1,12 +1,25 @@
 import { Client } from "../client/Client.ts";
+import { Resolver } from "../util/Resolver.ts";
 
-interface ChannelStructure {
-  id: string;
-}
+export type ChannelTypes =
+  | "GUILD_TEXT"
+  | "DM"
+  | "GUILD_VOICE"
+  | "GROUP_DM"
+  | "GUILD_CATEGORY"
+  | "GUILD_NEWS"
+  | "GUILD_STORE";
 
 export class Channel {
-  readonly id: string;
-  constructor(structure: ChannelStructure, public client: Client) {
+  /** the id of this channel */
+  public readonly id: string;
+  /** the type of channel */
+  public type: ChannelTypes;
+  constructor(structure: any, public client: Client) {
     this.id = structure.id;
+    this.type = Resolver.toHumanReadableChannelType(structure.type);
   }
 }
+
+// todo(n1c00o): Channel Structure
+// ! Please don't touch to this file
