c84422911a12d4cdaa64e91366f836eebb3470f5
Cat66000 <none>
2020-07-23 23:08:44 +0200

    Refactor UserManager

--- src/managers/UserManager.ts
+++ src/managers/UserManager.ts
@@ -1,35 +1,37 @@
 import { Client } from "../client/Client.ts";
 import { User } from "../models/User.ts";
-import { CDN_BASE_URL } from "../util/Constants.ts";
+import { USER_AVATAR, USER_DEFAULT_AVATAR } from "../util/endpoints/CDN.ts";
 
-type AllowedFormats = "webp" | "gif" | "png" | "jpg" | "jpeg";
-type AllowedSizes = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
+export type AllowedFormats = "webp" | "gif" | "png" | "jpg" | "jpeg";
+export type AllowedSizes = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
 
 export class UserManager {
-  constructor(public client: Client) {
-  }
+  constructor(public client: Client) {}
 
-  public getAvatarURL(user: User, params: {
-    size?: AllowedSizes;
+  public getAvatarURL(user: User, options: {
     format?: AllowedFormats;
-    canBeDynamic?: boolean;
+    size?: AllowedSizes;
+    dynamic?: boolean;
   } = {
-    size: 256,
     format: "webp",
-    canBeDynamic: false,
+    size: 256,
+    dynamic: false,
   }): string {
     if (user.isPartial) throw new Error("User should not be partial.");
-    return `${CDN_BASE_URL}/avatars/${user.id}/${user.avatarHash}.${
-      params.canBeDynamic && user.avatarHash?.startsWith("a_")
-        ? "gif"
-        : params.format
-    }?size=${params.size}`;
+
+    return USER_AVATAR(
+      {
+        userID: user.id,
+        avatarHash: user.avatarHash as string,
+        format: options.format,
+        size: options.size,
+        dynamic: options.dynamic,
+      },
+    );
   }
 
   public getDefaultAvatarURL(user: User, size: AllowedSizes): string {
     if (user.isPartial) throw new Error("User should bot be partial.");
-    return `${CDN_BASE_URL}/embed/avatars/${user.id}/${Number(
-      user.discriminator,
-    ) % 5}.png?size=${String(size)}`;
+    return USER_DEFAULT_AVATAR(user.discriminator as string);
   }
 }
