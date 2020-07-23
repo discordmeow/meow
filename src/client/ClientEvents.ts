438c378f794a399a189f548d64ed359e1b3aa234
Cat66000 <none>
2020-07-24 00:30:18 +0200

    Add guildCreate, guildAvailable/Unavailable events

--- src/client/ClientEvents.ts
+++ src/client/ClientEvents.ts
@@ -1,7 +1,11 @@
 import { Evt } from "../../deps.ts";
 import { GatewayError } from "../errors/GatewayError.ts";
+import { Guild } from "../models/Guild.ts";
 
 export class ClientEvents {
   public readonly ready = new Evt<void>();
   public readonly gatewayError = new Evt<GatewayError>();
+  public readonly guildCreate = new Evt<Guild>();
+  public readonly guildAvailable = new Evt<Guild>();
+  public readonly guildUnavailable = new Evt<Guild>();
 }
