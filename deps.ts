21870bfcc538e6d68a89461bdeeff036576ec114
Cat66000 <none>
2020-07-23 18:34:35 +0200

    Modify deps.ts

--- deps.ts
+++ deps.ts
@@ -1,8 +1,7 @@
 export {
   WebSocket,
+  WebSocketCloseEvent,
   connectWebSocket,
   isWebSocketCloseEvent,
-  isWebSocketPingEvent,
-  isWebSocketPongEvent,
 } from "https://deno.land/std@0.61.0/ws/mod.ts";
 export { Evt } from "https://deno.land/x/evt@1.7.14/mod.ts";
