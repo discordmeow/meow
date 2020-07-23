e57b208ae5c41e200d2f6681d09664eaa897c84c
Cat66000 <none>
2020-07-24 00:38:01 +0200

    Handle GUILD_CREATE event Handle lazy-loading of guilds at startup Add enum EventTypes

--- src/network/EventHandler.ts
+++ src/network/EventHandler.ts
@@ -1,19 +1,89 @@
 import { WebSocketHandler } from "./WebSocketHandler.ts";
 import { Client } from "../client/Client.ts";
+import { Guild } from "../models/Guild.ts";
 
 export interface EventData {
   name: string;
   data: any;
 }
 
+export enum EventTypes {
+  READY = "READY",
+  RESUMED = "RESUMED",
+  CHANNEL_CREATE = "CHANNEL_CREATE",
+  CHANNEL_UDPATE = "CHANNEL_UPDATE",
+  CHANNEL_DELETE = "CHANNEL_DELETE",
+  CHANNEL_PINS_UPDATE = "CHANNEL_PINS_UPDATE",
+  GUILD_CREATE = "GUILD_CREATE",
+  GUILD_UPDATE = "GUILD_UPDATE",
+  GUILD_DELETE = "GUILD_DELETE",
+  GUILD_BAN_ADD = "GUILD_BAN_ADD",
+  GUILD_BAN_REMOVE = "GUILD_BAN_REMOVE",
+  GUILD_EMOJIS_UPDATE = "GUILD_EMOJIS_UPDATE",
+  GUILD_INTEGRATIONS_UPDATE = "GUILD_INTEGRATIONS_UPDATE",
+  GUILD_MEMBER_ADD = "GUILD_MEMBER_ADD",
+  GUILD_MEMBER_REMOVE = "GUILD_MEMBER_REMOVE",
+  GUILD_MEMBER_UPDATE = "GUILD_MEMBER_UPDATE",
+  GUILD_MEMBERS_CHUNK = "GUILD_MEMBERS_CHUNK",
+  GUILD_ROLE_CREATE = "GUILD_ROLE_CREATE",
+  GUILD_ROLE_UPDATE = "GUILD_ROLE_UPDATE",
+  GUILD_ROLE_DELETE = "GUILD_ROLE_DELETE",
+  INVITE_CREATE = "INVITE_CREATE",
+  INVITE_DELETE = "INVITE_DELETE",
+  MESSAGE_CREATE = "MESSAGE_CREATE",
+  MESSAGE_UPDATE = "MESSAGE_UPDATE",
+  MESSAGE_DELETE = "MESSAGE_DELETE",
+  MESSAGE_DELETE_BULK = "MESSAGE_DELETE_BULK",
+  MESSAGE_REACTION_ADD = "MESSAGE_REACTION_ADD",
+  MESSAGE_REACTION_REMOVE = "MESSAGE_REACTION_REMOVE",
+  MESSAGE_REACTION_REMOVE_ALL = "MESSAGE_REACTION_REMOVE_ALL",
+  MESSAGE_REACTION_REMOVE_EMOJI = "MESSAGE_REACTION_REMOVE_EMOJI",
+  PRESENCE_UPDATE = "PRESENCE_UPDATE",
+  TYPING_START = "TYPING_START",
+  USER_UPDATE = "USER_UPDATE",
+  VOICE_STATE_UPDATE = "VOICE_STATE_UPDATE",
+  VOICE_SERVER_UPDATE = "VOICE_SERVER_UPDATE",
+  WEBHOOKS_UPDATE = "WEBHOOKS_UPDATE",
+}
+
 export class EventHandler {
   constructor(public ws: WebSocketHandler, public client: Client) {}
 
-  public handle(event: EventData) {
-    switch (event.name) {
-      case "READY":
-        this.ws.sessionID = event.data.session_id;
+  public handle({ name, data }: EventData) {
+    switch (name) {
+      case EventTypes.READY:
+        this.ws.sessionID = data.session_id;
+
+        (data.guilds as Guild[]).forEach((guild) => {
+          this.client.cache.cacheGuild(data);
+          this.ws.initialUnavailableGuilds.add(guild.id);
+        });
+
         this.client.events.ready.post();
+        break;
+      case EventTypes.GUILD_CREATE:
+        const guild = this.client.cache.cacheGuild(data);
+
+        if (
+          !this.client.cache.unavailableGuilds.has(data.id) &&
+          !data.unavailable && !this.ws.initialUnavailableGuilds.has(data.id)
+        ) {
+          this.client.events.guildCreate.post(guild);
+        } else if (
+          this.client.cache.unavailableGuilds.has(data.id) && !data.unavailable
+        ) {
+          this.client.cache.unavailableGuilds.delete(data.id);
+
+          this.client.events.guildAvailable.post(guild);
+        } else if (data.unavailable) {
+          this.client.cache.unavailableGuilds.add(data.id);
+
+          this.client.events.guildUnavailable.post(guild);
+        }
+
+        if (this.ws.initialUnavailableGuilds.has(data.id)) {
+          this.ws.initialUnavailableGuilds.delete(data.id);
+        }
         break;
     }
   }
