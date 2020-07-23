82384b6c35c3e183ba24fe4dea635a75be106978
n1c00o <34602094+Nicoxx45@users.noreply.github.com>
2020-07-23 20:36:53 +0200

    Fixed emoji.user and emoji.roles

--- src/models/Emoji.ts
+++ src/models/Emoji.ts
@@ -9,7 +9,7 @@ export class Emoji {
   /** Emoji name */
   public name: string;
   /** User that created this emoji */
-  // public user!: User;
+  public user!: User | null;
   /** Whether this emoji is managed */
   public managed: boolean;
   /** Whether this emoji is animated */
@@ -17,25 +17,25 @@ export class Emoji {
   /** Whether this emoji can be used, may be false due to loss of Server Boosts */
   public available: boolean;
   /** Roles this emoji is whitelist to */
-  // public roles: Map<string, Role> = new Map<string, Role>();
+  public roles: Map<string, Role> = new Map<string, Role>();
   /** Whether this emoji must be wrapped in colons */
   public requireColons: boolean;
 
   constructor(structure: any, public guild: Guild, public client: Client) {
     this.id = structure.id;
     this.name = structure.name;
-    // if (structure.user) this.user = structure.user;
+    if (structure.user) this.user = client.cache.cacheUser(structure.user);
     this.managed = structure.managed;
     this.animated = structure.animated;
     this.available = structure.available;
     this.requireColons = structure.require_colons;
 
-    /*
     if (structure.roles) {
       structure.roles.map((role: any): void => {
-        this.roles.set(role.id, new Role(role, guild, client));
+        if (guild.roles.has(role.id)) 
+          this.roles.set(role.id, <Role>guild.roles.get(role.id));
       });
     } 
-    */
+    
   }
 }
