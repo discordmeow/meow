3d34c9c6c77b3834280bb7e00cc086c758cd3e61
Cat66000 <none>
2020-07-23 03:17:05 +0200

    Update Client to be compatible with the last commits

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,11 +1,33 @@
 import { UserManager } from "../managers/UserManager.ts";
 import { WebSocketHandler } from "../network/WebSocketHandler.ts";
-import * as eventsHandlers from "./Events.ts";
+import { Events } from "./Events.ts";
+
+export interface ClientOptions {
+  token: string;
+}
 
 export class Client {
-  readonly users = new UserManager(this);
-  readonly ws = new WebSocketHandler(this);
-  events = eventsHandlers;
+  public readonly token: string;
+  public readonly users = new UserManager(this);
+  public readonly ws = new WebSocketHandler(this);
+  public readonly events = new Events();
 
-  constructor() {}
+  constructor(public options: ClientOptions) {
+    if (!options?.token) {
+      throw new Error("Client cannot be instanciated without a token");
+    }
+
+    options.token = `Bot ${options.token}`;
+
+    this.token = options.token;
+  }
+
+  async connect() {
+    await this.ws.handleConnecting();
+  }
 }
+
+const client = new Client(
+  { token: "NzM1NTQ4OTE3MzQwNzAwNzAz.XxjVAA.7qXvYxLS9fR57_om2XqoojqPaIk" },
+);
+await client.connect();
