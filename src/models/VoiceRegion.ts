462e47724e1b407bbd3ed438526a6a00627e6b68
n1c00o <34602094+Nicoxx45@users.noreply.github.com>
2020-07-23 20:37:50 +0200

    VoiceRegion model

---
+++ src/models/VoiceRegion.ts
@@ -0,0 +1,24 @@
+import { Client } from "../client/Client.ts";
+
+export class VoiceRegion {
+  /** unique ID for the region */
+  public readonly id: string;
+  /** name of the region */
+  public name: string;
+  /** `true` if this is a vip-only server */
+  public vip: boolean;
+  /** `true` for a single server that is closest to the current user's client */
+  public optimal: boolean;
+  /** whether this is a deprecated voice region (avoid switching to these) */
+  public deprecated: boolean;
+  /** whether this is a custom voice region (used for events/etc) */
+  public custom: boolean;
+  constructor(structure: any, public client: Client) {
+    this.id = structure.id;
+    this.name = structure.name;
+    this.vip = structure.vip;
+    this.optimal = structure.optimal;
+    this.deprecated = structure.deprecated;
+    this.custom = structure.custom;
+  }
+}
