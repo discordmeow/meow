710966dd6e234f6c0331d40cb55397189a1736f3
Cat66000 <none>
2020-07-24 13:46:45 +0200

    Bump std to 0.62.0 and evt to 1.8.0

--- deps.ts
+++ deps.ts
@@ -3,5 +3,5 @@ export {
   WebSocketCloseEvent,
   connectWebSocket,
   isWebSocketCloseEvent,
-} from "https://deno.land/std@0.61.0/ws/mod.ts";
-export { Evt } from "https://deno.land/x/evt@1.7.14/mod.ts";
+} from "https://deno.land/std@0.62.0/ws/mod.ts";
+export { Evt } from "https://deno.land/x/evt@1.8.0/mod.ts";
