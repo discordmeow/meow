4ddcf4fff78caa47f7c3d7c84807d0608456f83f
Cat66000 <none>
2020-07-25 17:16:55 +0200

    Refactor GuildMember Add CacheManager#patchMember

--- src/models/GuildMember.ts
+++ src/models/GuildMember.ts
@@ -1,6 +1,7 @@
 import { Client } from "../client/Client.ts";
 import { Guild } from "./Guild.ts";
 import { User } from "./User.ts";
+import { RawGuildMember } from "../network/event_handling/RawStructures.ts";
 
 export class GuildMember {
   /** the user this guild member represents */
@@ -8,22 +9,24 @@ export class GuildMember {
   /** this users guild nickname */
   public nick?: string;
   /** roles id of this guild member */
-  public roles: string[];
+  public roles!: string[];
   /** when the user joined the guild */
   public joinedAt: number;
   /** when the user started boosting the guild */
   public premiumSince?: number | null;
   /** whether the user is deafened in voice channels */
-  public deaf: boolean;
+  public deaf!: boolean;
   /** whether the user is deafened in voice channels */
-  public mute: boolean;
-  constructor(structure: any, public guild: Guild, public client: Client) {
+  public mute!: boolean;
+
+  constructor(
+    structure: RawGuildMember,
+    public guild: Guild,
+    public client: Client,
+  ) {
     if (structure.user) this.user = client.cache.addUser(structure.user);
-    this.nick = structure.nick;
-    this.roles = structure.roles;
     this.joinedAt = structure.joined_at;
-    if (structure.premium_since) this.premiumSince = structure.premium_since;
-    this.deaf = structure.deaf;
-    this.mute = structure.mute;
+
+    client.cache.patchMember(this, structure);
   }
 }
