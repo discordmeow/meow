7ddcb2c199d7e50a28c199a72267e2ed001c33df
Cat66000 <none>
2020-07-27 22:43:06 +0200

    Add Activity & ActivityAssets

---
+++ src/models/Activity.ts
@@ -0,0 +1,50 @@
+import {
+  RawActivity,
+  RawActivityTypes,
+  RawActivityTimestamps,
+  RawActivityParty,
+  RawActivitySecrets,
+  RawActivityFlags,
+} from "../network/event_handling/RawStructures.ts";
+import { Client } from "../client/Client.ts";
+import { GuildEmoji } from "./GuildEmoji.ts";
+import { ActivityAssets } from "./ActivityAssets.ts";
+
+export type ActivityTypes = RawActivityTypes;
+export type ActivityTimestamps = RawActivityTimestamps;
+export type ActivityParty = RawActivityParty;
+export type ActivitySecrets = RawActivitySecrets;
+export type ActivityFlags = RawActivityFlags;
+
+export class Activity {
+  /** the activity's name */
+  public name!: string;
+  /** activity type */
+  public type!: ActivityTypes;
+  /** stream url, is validated when type is 1 */
+  public url?: string | null;
+  /** unix timestamp of when the activity was added to the user's session */
+  public createdAt!: number;
+  /** unix timestamps for start and/or end of the game */
+  public timestamps?: ActivityTimestamps;
+  /** application id for the game */
+  public applicationID?: string;
+  /** what the player is currently doing */
+  public details?: string | null;
+  /** the user's current party status */
+  public state?: string | null;
+  /** the emoji used for a custom status */
+  public emoji?: GuildEmoji | null;
+  /** information for the current party of the player */
+  public party?: ActivityParty;
+  /** images for the presence and their hover texts */
+  public assets?: ActivityAssets;
+  /** secrets for Rich Presence joining and spectating */
+  public secrets?: ActivitySecrets;
+  /** whether or not the activity is an instanced game session */
+  public instance?: boolean;
+  /** activity flags */
+  public flags?: ActivityFlags;
+
+  constructor(structure: RawActivity, public client: Client) {}
+}
