fb0316540a5b9988c2d76a0448215b8ac0b2eefb
Cat66000 <36672371+Cat66000@users.noreply.github.com>
2020-08-22 01:01:05 +0200

    Refactor(Resolver): Rename methods

--- src/util/Resolver.ts
+++ src/util/Resolver.ts
@@ -12,17 +12,17 @@ export class Resolver {
   ];
 
   /** Convert an API channel.type to a human readable one */
-  public static toHumanReadableChannelType(type: number): ChannelTypes {
+  public static toStringChannelType(type: number): ChannelTypes {
     if (!this._channelTypes[type]) {
-      throw new RangeError("'type' must be between 0 and 6!");
+      throw new RangeError("`type` must be between 0 and 6!");
     }
     return this._channelTypes[type];
   }
 
   /** Convert a human readable channel type to the API index */
-  public static toAPIChannelType(type: ChannelTypes): number {
+  public static toNumberChannelType(type: ChannelTypes): number {
     if (!this._channelTypes.includes(type)) {
-      throw new TypeError("'type' must be a valid ChannelType!");
+      throw new TypeError("`type` must be a valid ChannelType!");
     }
     return this._channelTypes.indexOf(type);
   }
