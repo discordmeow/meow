7bf54a56f6244a4a9fca1b5d89a5b4dd65cb15e0
Cat66000 <36672371+Cat66000@users.noreply.github.com>
2020-08-23 19:30:40 +0200

    feat(Constants): Add API_VERSION

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "https://discord.com/api/v7";
+export const API_BASE_URL = "https://discord.com/api/";
+export const API_VERSION = "v7";
 
 export const CDN_BASE_URL = "https://cdn.discordapp.com";
 
