8d472dae8fefc7336b5bbcae8b617bdc5c5a0bed
Cat66000 <36672371+Cat66000@users.noreply.github.com>
2020-08-28 01:27:55 +0200

    refactor(Cache): Fix everything Cache#patch{Emoji => GuildEmoji}

--- src/client/Cache.ts
+++ src/client/Cache.ts
@@ -12,6 +12,7 @@ import {
   RawGuildMember,
   RawPresenceUpdate,
   RawActivity,
+  RawVoiceState,
 } from "../util/RawStructures.ts";
 import { GuildMember } from "../models/GuildMember.ts";
 import { VoiceState } from "../models/VoiceState.ts";
@@ -21,81 +22,98 @@ import { Resolver } from "../util/Resolver.ts";
 
 export class Cache {
   /** Map containing every cached users */
-  public users: Map<string, User> = new Map();
+  public users = new Map<string, User>();
   /** Map containing every cached guilds */
-  public guilds: Map<string, Guild> = new Map();
+  public guilds = new Map<string, Guild>();
   /** Map containing every cached emojis */
-  public emojis: Map<string, GuildEmoji> = new Map();
+  public emojis = new Map<string, GuildEmoji>();
   /** Map containing every cached channels */
-  public channels: Map<string, Channel> = new Map();
+  public channels = new Map<string, Channel>();
   /** Set containing IDs of unavailable guilds */
-  public unavailableGuilds: Set<string> = new Set();
+  public unavailableGuilds = new Set<string>();
 
   constructor(public client: Client) {}
 
   /** Cache an User */
   public addUser(structure: RawUser): User {
-    const cached: User | undefined = this.users.get(structure.id);
+    const cached = this.users.get(structure.id);
     if (cached) return this.patchUser(cached, structure);
 
-    const toCache: User = new User(structure, this.client);
+    const toCache = new User(structure, this.client);
     this.users.set(toCache.id, toCache);
     return toCache;
   }
 
   /** Update a cached user */
   public patchUser(user: User, structure: RawUser): User {
-    if (structure.username) user.username = structure.username;
-    if (structure.discriminator) user.discriminator = structure.discriminator;
-    if (structure.avatar) user.avatar = structure.avatar;
-    if (structure.bot) user.bot = Boolean(structure.bot);
-    if (structure.system) user.system = Boolean(structure.system);
-    if (structure.mfa_enabled) user.mfaEnabled = Boolean(structure.mfa_enabled);
-    if (structure.locale) user.locale = structure.locale;
-    if (structure.premium_type) user.premiumType = structure.premium_type;
-    if (structure.public_flags) user.publicFlags = structure.public_flags;
+    user.avatar = structure.avatar;
+
+    if (typeof structure.premium_type !== "undefined") {
+      user.premiumType = structure.premium_type;
+    }
+    if (typeof structure.mfa_enabled !== "undefined") {
+      user.mfaEnabled = structure.mfa_enabled;
+    }
+    if (typeof structure.system !== "undefined") {
+      user.system = structure.system;
+    }
+    if (typeof structure.public_flags !== "undefined") {
+      user.publicFlags = structure.public_flags;
+    }
+    if (typeof structure.locale !== "undefined") user.locale = structure.locale;
+    if (typeof structure.bot !== "undefined") user.bot = structure.bot;
+
+    if (structure.username && (structure.username !== user.username)) {
+      user.username = structure.username;
+    }
+    if (
+      structure.discriminator &&
+      (structure.discriminator !== user.discriminator)
+    ) {
+      user.discriminator = structure.discriminator;
+    }
 
     return user;
   }
 
   /** Cache an Emoji */
   public addEmoji(structure: RawEmoji, guild: Guild): GuildEmoji {
-    const cached: GuildEmoji | undefined = this.emojis.get(
+    const cached = this.emojis.get(
       structure.id as string,
     );
-    if (cached) return this.patchEmoji(cached, structure);
+    if (cached) return this.patchGuildEmoji(cached, structure);
 
-    const toCache: GuildEmoji = new GuildEmoji(structure, guild, this.client);
+    const toCache = new GuildEmoji(structure, guild, this.client);
     this.emojis.set(toCache.id, toCache);
+    guild.emojis.set(toCache.id, toCache);
 
     return toCache;
   }
 
   /** Update a cached Emoji */
-  public patchEmoji(emoji: GuildEmoji, structure: RawEmoji): GuildEmoji {
-    if (structure.name) emoji.name = structure.name;
+  public patchGuildEmoji(emoji: GuildEmoji, structure: RawEmoji): GuildEmoji {
+    const guild = emoji.guild();
 
-    if (structure.managed) emoji.managed = structure.managed;
-    if (structure.animated) emoji.animated = structure.animated;
-    if (structure.available) emoji.available = structure.available;
-    if (structure.require_colons) {
-      emoji.requireColons = structure.require_colons;
-    }
-    if (structure.roles) {
-      structure.roles.forEach((roleID) => {
-        emoji.roles.set(roleID, emoji.guild().roles.get(roleID) as Role);
-      });
-    }
+    emoji.name = structure.name as string;
+
+    emoji.managed = structure.managed as boolean;
+    emoji.animated = structure.animated as boolean;
+    emoji.available = structure.available as boolean;
+    emoji.requireColons = structure.require_colons as boolean;
+
+    ((structure.roles) as string[]).forEach((roleID) => {
+      emoji.roles.set(roleID, guild.roles.get(roleID) as Role);
+    });
 
     return emoji;
   }
 
   /** Cache a Guild */
   public addGuild(structure: RawGuild): Guild {
-    const cached: Guild | undefined = this.guilds.get(structure.id);
+    const cached = this.guilds.get(structure.id);
     if (cached) return this.patchGuild(cached, structure);
 
-    const toCache: Guild = new Guild(structure, this.client);
+    const toCache = new Guild(structure, this.client);
     this.guilds.set(toCache.id, toCache);
 
     return toCache;
@@ -110,7 +128,9 @@ export class Cache {
     if (structure.afk_timeout !== guild.afkTimeout) {
       guild.afkTimeout = structure.afk_timeout;
     }
-    guild.verificationLevel = structure.verification_level;
+    if (structure.verification_level !== guild.verificationLevel) {
+      guild.verificationLevel = structure.verification_level;
+    }
     guild.defaultMessageNotifications = structure.default_message_notifications;
     guild.explicitContentFilter = structure.explicit_content_filter;
     guild.features = structure.features;
@@ -118,59 +138,78 @@ export class Cache {
 
     if (structure.region !== guild.region) guild.region = structure.region;
 
-    if (Boolean(structure.owner) !== Boolean(guild.owner)) {
+    if (
+      typeof structure.owner !== "undefined" &&
+      (structure.owner !== guild.owner)
+    ) {
       guild.owner = structure.owner;
     }
 
-    if (structure.icon) guild.icon = structure.icon;
-    if (structure.splash) guild.splash = structure.splash;
-    if (structure.discovery_splash) {
+    if (structure.icon !== guild.icon) guild.icon = structure.icon;
+    if (structure.splash && (structure.splash !== guild.splash)) {
+      guild.splash = structure.splash;
+    }
+    if (structure.discovery_splash && (structure.splash !== guild.splash)) {
       guild.discoverySplash = structure.discovery_splash;
     }
-    if (structure.permissions) guild.permissions = structure.permissions;
-    if (structure.afk_channel_id) guild.afkChannelID = structure.afk_channel_id;
+    if (
+      typeof structure.permissions !== "undefined" &&
+      (structure.permissions !== guild.permissions)
+    ) {
+      guild.permissions = structure.permissions;
+    }
+    if (structure.afk_channel_id !== guild.afkChannelID) {
+      guild.afkChannelID = structure.afk_channel_id;
+    }
 
-    if (structure.application_id) {
+    if (structure.application_id !== guild.applicationID) {
       guild.applicationID = structure.application_id;
     }
-    if (structure.member_count) guild.memberCount = structure.member_count;
+    if (
+      typeof structure.member_count !== "undefined" &&
+      (structure.member_count !== guild.memberCount)
+    ) {
+      guild.memberCount = structure.member_count;
+    }
 
     return guild;
   }
 
   public loadFullGuild(guild: Guild, structure: RawGuild) {
-    structure.roles.map((role): void => {
+    structure.roles.forEach((role): void => {
       guild.roles.set(role.id, new Role(role, guild, this.client));
     });
-    structure.emojis.map((emoji): void => {
-      guild.emojis.set(emoji.id as string, this.addEmoji(emoji, guild));
+    structure.emojis.forEach((emoji): void => {
+      this.addEmoji(emoji, guild);
     });
 
-    structure.voice_states?.map((voiceState): void => {
-      guild.voiceStates.push(new VoiceState(voiceState, guild, this.client));
-    });
+    guild.voiceStates = ((structure.voice_states) as RawVoiceState[]).map((
+      voiceState,
+    ): VoiceState => new VoiceState(voiceState, guild, this.client));
 
-    structure.members?.map((member): void => {
+    structure.members?.forEach((member): void => {
       guild.members.set(
         ((member.user) as RawUser).id,
         new GuildMember(member, guild, this.client),
       );
     });
 
-    structure.channels?.map((channel): void => {
-      guild.channels.set(channel.id, this.addChannel(channel));
+    structure.channels?.forEach((channel): void => {
+      this.addChannel(channel, guild);
     });
 
     return guild;
   }
 
   /** Cache a Channel */
-  public addChannel(structure: RawChannel): Channel {
-    const cached: Channel | undefined = this.channels.get(structure.id);
+  public addChannel(structure: RawChannel, guild?: Guild): Channel {
+    const cached = this.channels.get(structure.id);
     if (cached) return this.patchChannel(cached, structure);
 
-    const toCache: Channel = new Channel(structure, this.client);
+    const toCache = new Channel(structure, this.client);
     this.channels.set(toCache.id, toCache);
+    guild?.channels.set(toCache.id, toCache);
+
     return toCache;
   }
 
@@ -210,8 +249,8 @@ export class Cache {
   public patchActivity(activity: Activity, structure: RawActivity) {
     activity.url = structure.url;
 
-    if (activity.name !== structure.name) activity.name = structure.name;
-    if (activity.type !== structure.type) activity.type = structure.type;
+    if (structure.name !== activity.name) activity.name = structure.name;
+    if (structure.type !== activity.type) activity.type = structure.type;
 
     if (activity.createdAt !== structure.created_at) {
       activity.createdAt = structure.created_at;
@@ -250,6 +289,7 @@ export class Cache {
     presence.clientStatus = structure.client_status;
     presence.status = structure.status;
     presence.rolesID = structure.roles;
+
     if (presence.game) {
       if (structure.game) {
         this.patchActivity(presence.game, structure.game);
@@ -263,7 +303,7 @@ export class Cache {
         presence.game = undefined;
       }
     }
-    presence.activities = structure.activities.map((activity) =>
+    presence.activities = structure.activities.map((activity): Activity =>
       new Activity(activity, this.client)
     );
   }
