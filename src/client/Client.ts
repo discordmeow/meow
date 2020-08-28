6a22418e0f02489613d216ef1f6131d081eb2512
Cat66000 <36672371+Cat66000@users.noreply.github.com>
2020-08-29 00:18:10 +0200

    refactor(Client): getAvatarURL

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -7,6 +7,12 @@ import { USER_AVATAR, USER_DEFAULT_AVATAR } from "../util/endpoints/CDN.ts";
 export type AllowedFormats = "webp" | "gif" | "png" | "jpg" | "jpeg";
 export type AllowedSizes = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
 
+export interface AvatarOptions {
+  format?: AllowedFormats;
+  size?: AllowedSizes;
+  dynamic?: boolean;
+}
+
 export interface ClientOptions {
   token: string;
 }
@@ -31,16 +37,16 @@ export class Client {
     await this.ws.connect();
   }
 
-  public getAvatarURL(user: User, options: {
-    format?: AllowedFormats;
-    size?: AllowedSizes;
-    dynamic?: boolean;
-  } = {
+  public getAvatarURL(user: User, options: AvatarOptions = {
     format: "webp",
     size: 256,
     dynamic: false,
-  }): string {
-    if (user.isPartial) throw new Error("User should not be partial.");
+  }): string | undefined {
+    if (!user.avatar) return;
+
+    if (!options.format) options.format = "webp";
+    if (!options.size) options.size = 256;
+    if (typeof options.dynamic !== "undefined") options.dynamic = false;
 
     return USER_AVATAR(
       {
@@ -53,7 +59,7 @@ export class Client {
     );
   }
 
-  public getDefaultAvatarURL(user: User, size: AllowedSizes): string {
+  public getDefaultAvatarURL(user: User): string {
     if (user.isPartial) throw new Error("User should not be partial.");
     return USER_DEFAULT_AVATAR(user.discriminator as string);
   }
