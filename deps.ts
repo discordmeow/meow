c38b58ee2af1f5ba06786d981f4f345e875c27c4
Cat66000 <36672371+Cat66000@users.noreply.github.com>
2020-10-08 16:38:29 +0200

    Update evt to 1.8.10

--- deps.ts
+++ deps.ts
@@ -4,4 +4,4 @@ export {
   connectWebSocket,
   isWebSocketCloseEvent,
 } from "https://deno.land/std@0.66.0/ws/mod.ts";
-export { Evt } from "https://deno.land/x/evt@v1.8.7/mod.ts";
+export { Evt } from "https://deno.land/x/evt@v1.8.10/mod.ts";
